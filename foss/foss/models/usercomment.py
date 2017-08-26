8aceed3babcfe0e40b080941f25496db34328508
Supraja T R D <suprajadinakaran@gmail.com>
2017-08-26 19:50:48 +0530

    Add files via upload

---
+++ foss/foss/models/usercomment.py
@@ -0,0 +1,19 @@
+from sqlalchemy import (
+    Column,
+    Index,
+    Integer,
+    Text,
+)
+
+from .meta import Base
+
+
+class Comment(Base):
+    __tablename__ = 'Comment'
+    id = Column(Integer, primary_key=True)
+    email = Column(Text)
+    topic = Column(Text)
+    comment = Column(Text)
+    datetime = Column(Text)
+    
+
